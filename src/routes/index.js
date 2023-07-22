f10ade5f1b2f5978c6958a99e9f0f41c29d44944
MRaehanR <raehanrobban2@gmail.com>
2023-07-22 19:45:50 +0700

    fix: route not found response

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,8 @@ import apiRoutes from "./api/index.js";
 const router = express.Router();
 
 router.use("/api", apiRoutes);
+router.use("*", (req, res) => {
+  res.error("Route Not Found", 404);
+});
 
 export default router;
